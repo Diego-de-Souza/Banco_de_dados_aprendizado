1849d92bdb6b4503b07c8dcea7e3f35e313b544b
Diego de Souza <49752675+Diego-de-Souza@users.noreply.github.com>
2023-08-04 22:40:54 -0300

    implementação do exercicio DB2

--- exercicios/exercicio_BD2_20230803.sql
+++ exercicios/exercicio_BD2_20230803.sql
@@ -1,9 +1,17 @@
 /*
-Exercico de RevisÃ£o
+Exercico de Revisão
 
-1) Liste o numero da ordem, sua data de criaÃ§Ã£o e o nome do cliente
+1) Liste o numero da ordem, sua data de criação e o nome do cliente
 
 2) Liste o nome de todos os empregados que moram no territorio de "Providence"
+
+3) Quantos empregados residem em cada cidade
+
+4)Exiba o nome do cliente (costumer) e a quantidade de pedidos
+(orders) que cada um póssui. Faça isso ordenado
+pela quantidade decrescente
+
+5) Liste o nome de todos os produtos que nunca foram vendidos
 */
 
 select o.OrderID, o.OrderDate, c.ContactName
@@ -16,3 +24,19 @@ from Employees em
 inner join EmployeeTerritories et on (et.EmployeeID = em.EmployeeID)
 inner join Territories te on (te.TerritoryID = et.TerritoryID)
 where TerritoryDescription = 'Providence'
+
+select COUNT(*) Qtd, City
+from Employees
+group by City
+
+select Cu.ContactName, COUNT(o.OrderID)Qtd
+from Customers Cu
+left join Orders o on (o.CustomerID = Cu.CustomerID)
+group by Cu.ContactName
+order by Qtd desc
+
+select p.ProductName
+from Products p
+left join [Order Details] od 
+	on (od.ProductID = p.ProductID)
+where od.OrderID is null 
