e7a74236bb36abcfd3801da8ca4ab8e8ca8ff873
Diego de Souza <49752675+Diego-de-Souza@users.noreply.github.com>
2023-08-10 20:52:59 -0300

    exercicio de BD2

--- exercicios/exercicio_BD2_20230810.sql
+++ exercicios/exercicio_BD2_20230810.sql
@@ -30,3 +30,38 @@ select p.ProductID, p.ProductName,
 	(select c.Description from Categories c
 	where p.CategoryID = c.CategoryID) Descricao
 from Products p
+
+------------------------------------------------------
+--com join
+select c.CategoryName, COUNT(p.ProductID) qtd
+from Categories c
+left join Products p
+	on c.CategoryID = p.CategoryID
+group by c.CategoryName
+
+--com select
+select c.CategoryName,
+	(select COUNT(*)
+	from Products p 
+	where p.CategoryID = c.CategoryID) qtd
+from Categories c
+
+---------------------------------------------------------------------------
+--usando subselect para restrição
+--Listar todas as categorias que possuem mais de 10 produtos
+
+--com join
+select c.CategoryName, COUNT(p.ProductID) qtd
+from Categories c
+left join Products p
+	on c.CategoryID = p.CategoryID
+group by c.CategoryName
+having count(p.ProductID)>10
+
+--com subselect
+select c.CategoryName,
+	(select COUNT(*) from Products p
+	where p.CategoryID = c.CategoryID) qtd
+from Categories c
+where (select COUNT(*) from Products p
+		where p.CategoryID = c.CategoryID)>10
