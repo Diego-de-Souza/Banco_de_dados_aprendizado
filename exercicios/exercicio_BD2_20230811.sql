f430f2c18a7b4dd9ce1b9e5bd220592c8d54aa01
DiegodeSouza <diegodesouza.souza@gmail.com>
2023-08-14 13:09:46 -0300

    exercicio BD2 20230811

--- exercicios/exercicio_BD2_20230811.sql
+++ exercicios/exercicio_BD2_20230811.sql
@@ -17,18 +17,24 @@ select p.ProductName, od.OrderID, od.Quantity
 from Products p
 inner join [Order Details] od on p.ProductID = od.ProductID
 
-select p.ProductName,
-	(select od.OrderID from [Order Details] od
-	where od.ProductID = p.ProductID)
+select od.OrderID, od.Quantity,
+	(select p.ProductName from Products p
+	where p.ProductID = od.ProductID)
+from [Order Details] od
 
-from Products p
-
+GO
 
 --3)
 select s.ContactName, count(p.ProductID) qtd
 from Suppliers s
 left join Products p on s.SupplierID = p.SupplierID
 group by s.ContactName
+
+select s.ContactName ,
+	(select count(p.ProductID) from Products p
+	where s.SupplierID = p.SupplierID) qtd
+from Suppliers s
+group by s.ContactName,s.SupplierID
 
 --4)
 select s.ContactName, count(p.ProductID) qtd
@@ -37,10 +43,46 @@ left join Products p on s.SupplierID = p.SupplierID
 group by s.ContactName
 having count(p.ProductID) > 2
 
+select s.ContactName ,
+	(select count(p.ProductID) from Products p
+	where p.SupplierID = s.SupplierID) qtd 
+from Suppliers s
+	where (select count(*) from Products p
+		where s.SupplierID = p.SupplierID) > 2
+
+
+
 --escreva os comandos para trazer os seguintes dados, escreva
 --utilizando join e utilizando subselect
 
 --5) traga o nome do territorio (territory) e o nome da região (region)
+select t.TerritoryDescription, r.RegionDescription
+from Territories t
+inner join Region r on t.RegionID = r.RegionID
+
+select t.TerritoryDescription,
+		(select r.RegionDescription from Region r
+		where t.RegionID = r.RegionID) RegionDescription
+from Territories t
 
 --6) exiba o nome de todos os funcionarios que a descrição do terrirorio
 --desse funcionario começa com a letra 'w'
+select e.FirstName
+from EmployeeTerritories et
+inner join Employees e on et.EmployeeID = e.EmployeeID
+inner join Territories t on et.TerritoryID = t.TerritoryID
+where t.TerritoryDescription like '%w%'
+
+select (select e.FirstName from Employees e
+       where e.EmployeeID = et.EmployeeID) nome
+from EmployeeTerritories et
+where (select t.TerritoryDescription from Territories t
+       where t.TerritoryID = et.TerritoryID) in
+	   (select t.TerritoryDescription from Territories t
+	   where t.TerritoryDescription like '%w%')
+
+select e.FirstName, t.TerritoryID, t.TerritoryDescription
+from EmployeeTerritories et
+inner join Employees e on e.EmployeeID = et.EmployeeID
+inner join Territories t on t.TerritoryID = et.TerritoryID
+where t.TerritoryDescription like '%w%'
